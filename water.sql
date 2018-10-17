a16c6755c43c5b0d73067adec0ad82b8d8c2bcf0
pushping <442829141@qq.com>
2018-10-17 21:27:06 +0800

    fxied

--- water.sql
+++ water.sql
@@ -7,7 +7,7 @@
 -- 服务器版本： 5.6.40-log
 -- PHP Version: 7.1.18
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO" ;
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
